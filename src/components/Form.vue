7312f209a0ae9c5084fa3caf593ba4a945a482d1
dvolkov <dvolkov@cmd.su>
2020-02-26 08:08:25 +0300

    Initial set up project

---
+++ src/components/Form.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Form template</h1>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: #203801;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Main template</h1>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: #42b983;
+}
+</style>
