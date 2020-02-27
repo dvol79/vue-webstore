8c42795548e9f94d300c68137e2c3be6a705c4de
dvolkov <dvolkov@cmd.su>
2020-02-27 22:09:21 +0300

    Add transition-animation

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -10,10 +10,10 @@
             </figure>
           </div>
           <div class="col-md-6 col-md-offset-0 description">
-            <router-link 
-              tag="h1" :to="{ name: 'Id', params: { id: product.id } }">
-              {{ product.title }}
-            </router-link>
+            <router-link
+              tag="h1"
+              :to="{ name: 'Id', params: { id: product.id } }"
+            >{{ product.title }}</router-link>
             <p v-html="product.description"></p>
             <p class="price">{{ product.price | formatPrice }}</p>
 
@@ -23,21 +23,25 @@
               v-if="canAddToCart(product)"
             >Add to cart</button>
             <button class="btn btn-default disabled" v-else>Add to cart</button>
-            <span
-              class="inventory-message"
-              v-if="product.availableInventory - cartCount(product.id) === 0"
-            >All out!</span>
-            <span
-              class="inventory-message"
-              v-else-if="product.availableInventory - cartCount(product.id) < 5"
-            >Only {{ product.availableInventory - cartCount(product.id) }} left!</span>
-            <span class="inventory-message" v-else>Buy now!</span>
-
+            <transition name="bounce" mode="out-in">
+              <span
+                class="inventory-message"
+                v-if="product.availableInventory - cartCount(product.id) === 0"
+                key ="0"
+              >All out!</span>
+              <span
+                class="inventory-message"
+                v-else-if="product.availableInventory - cartCount(product.id) < 5"
+                key =""
+              >Only {{ product.availableInventory - cartCount(product.id) }} left!</span>
+              <span class="inventory-message" v-else key ="">Buy now!</span>
+            </transition>
             <div class="rating">
-              <span 
+              <span
                 v-bind:class="{ 'rating-active': checkRating(n, product) }"
                 v-for="n in 5"
-                :key="n">☆</span>
+                :key="n"
+              >☆</span>
             </div>
           </div>
         </div>
@@ -49,7 +53,7 @@
 <script>
 import StoreHeader from "./Header.vue";
 export default {
-  name: 'Main',
+  name: "Main",
   data() {
     return {
       products: [],
@@ -118,7 +122,7 @@ export default {
     }
   },
   created: function() {
-    axios.get('/products.json').then(response => {
+    axios.get("/products.json").then(response => {
       this.products = response.data.products;
       console.log(this.products);
     });
@@ -127,7 +131,32 @@ export default {
 </script>
 
 <style scoped>
-h1 {
-  color: #42b983;
+.bounce-enter-active {
+  animation: shake 0.72s cubic-bezier(0.37, 0.07, 0.19, 0.97) both;
+  transform: translate(0, 0, 0);
+  backface-visibility: hidden;
+}
+
+@keyframes shake {
+  10%,
+  90% {
+    color: red;
+    transform: translate3d(-1px, 0, 0);
+  }
+  20%,
+  80% {
+    transform: translate3d(2px, 0, 0);
+  }
+  30%,
+  50%,
+  70% {
+    color: red;
+    transform: translate3d(-4px, 0, 0);
+  }
+  40%,
+  60% {
+    color: red;
+    transform: translate3d(4px, 0, 0);
+  }
 }
 </style>
